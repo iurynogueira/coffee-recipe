d1963f9755810cc9e23c2d3d365a2ffb6dfa64e8
Iury Nogueira <iunog@hotmail.com>
2023-03-16 11:03:37 -0300

    :sparkles: add vite project template

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Coffee Recipe</h1>
+      <div className="card">
+        <p className="read-the-docs">
+          Hello!
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
