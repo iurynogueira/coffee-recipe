25584f7f59329a51afe97a109e21ec9b4cc03e31
Iury Nogueira <iunog@hotmail.com>
2023-03-17 15:50:18 -0300

    :art: improve layout

--- src/App.tsx
+++ src/App.tsx
@@ -14,40 +14,48 @@ function App() {
   }
 
   return (
-    <div className='main-container'>
+    <div className='page-container'>
       <h1>Coffee Recipe</h1>
-      <div className='row-container'>
-        <div className='option-circle'>
-          <img src={Beans} alt="" style={{height: '40px'}}/>
-        </div>
+      <div className='inner-page'>
+        <div className='row-container'>
+          <div className='option-circle'>
+            <img src={Beans} alt="" style={{height: '40px'}}/>
+          </div>
 
-        <p className='read-the-docs'>Informe abaixo as proporções</p>
-      </div>
-
-      <div className='form-container'>
-        <span className='gram-value'>1g </span>
-        <span>para </span>
-        <div style={{gap: '10px', display: 'flex'}}>
-          {minGramOptions.map((value: number) => {
-            return (
-              <IonChip
-                handleClick={() => setMinMl(value)}
-                selected={minMl === value}
-                label={String(value)}
-                size={'md'}
-              />
-            )
-          })}
-          
+          <p className='read-the-docs'>Informe abaixo as proporções</p>
         </div>
+        <div className='main-container'>
+          <div className='form-container'>
+            <div className='form-inner'>
+              <span className='gram-value'>1g </span>
+              <span>para </span>
+              <div style={{gap: '10px', display: 'flex'}}>
+                {minGramOptions.map((value: number) => {
+                  return (
+                    <IonChip
+                      key={value}
+                      handleClick={() => setMinMl(value)}
+                      selected={minMl === value}
+                      label={String(value)}
+                      size={'md'}
+                    />
+                  )
+                })}
+              </div>
+            </div>
 
-        <div>
-          <p className='read-the-docs'>Quantas gramas deseja fazer?</p>
-        </div>
-        <input type="number" name="gram" value={gramRequired} onChange={(event) => handleChange(event.target.value, setGramRequired)} />
-        <div className='result-row'>
-          <span>Você pode fazer</span>
-          <span className='gram-value'>{gramRequired * minMl} mls</span>
+            <div className='form-inner'>
+              <div>
+                <p className='read-the-docs'>Quantas gramas deseja fazer?</p>
+              </div>
+              <input type="number" name="gram" value={gramRequired} onChange={(event) => handleChange(event.target.value, setGramRequired)} />
+              <div className='result-row'>
+                <span>Você pode fazer</span>
+                <span className='gram-value'>{gramRequired * minMl} mls</span>
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
